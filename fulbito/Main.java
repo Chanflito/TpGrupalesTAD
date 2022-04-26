ed23c4763fb0e059ca632be3e3f2ff96c1a10ecd
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2022-04-26 11:10:35 -0300

    EL FULBO ES PARA SIEMRPE

--- fulbito/Main.java
+++ fulbito/Main.java
@@ -1,14 +1,18 @@
 package Algoritmos.fulbito;
 
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String partidos= " 2 1\nBarcelona 1 \n Madrid 1 \n Barcelona Madrid";
+        String partidos= "2 1\nBarcelona 1,Madrid 1\nBarcelona Madrid";
         BruteForce hola= new BruteForce();
         System.out.println(Arrays.deepToString(hola.Leector(partidos)));
-        System.out.println(hola.get_partidos(partidos));
-        System.out.println(hola.get_equipos(partidos));
+        System.out.println(Arrays.deepToString(hola.getline1(partidos)));
+        System.out.println(hola.getEquipos(partidos));
+        System.out.println(hola.getPartidos(partidos));
+        System.out.println(Arrays.deepToString(hola.allteams1(partidos)));
+        System.out.println(Arrays.deepToString(hola.allmatches(partidos)));
 
     }
 }
--- fulbito/Strategy.java
+++ fulbito/Strategy.java
@@ -1,26 +1,52 @@
 package Algoritmos.fulbito;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Strategy {
+    String input;
     public void Algorithm(){
     }
     public String[] Leector(String input) {
-        String[] lines= input.split(System.getProperty("line.separator"));
+        String[] lines= input.split(("\\n"));
         return lines;
     }
-    public int get_partidos(String input){
-       String[] inputitos= Leector(input);
-       String nums=inputitos[0];
-       char numerito= nums.charAt(3);
-       return Character.getNumericValue(numerito);
-    }
-    public int get_equipos(String input){
-        String[] inputitos= Leector(input);
-        String nums=inputitos[0];
-        char numerito= nums.charAt(1);
-        return Character.getNumericValue(numerito);
-    }
 
+    public String[] getline1(String input){
+        String[] lines= Leector(input);
+        String[] line1=lines[0].split(" ");
+        return line1;
+    }
+    public int getPartidos(String input){
+        String[] line1=getline1(input);
+        return Integer.valueOf(line1[1]);
+    }
+    public int getEquipos(String input){
+        String [] line1=getline1(input);
+        return Integer.valueOf(line1[0]);
+    }
+    public String[] [] allteams1(String input){
+        String[] lines=Leector(input);
+        String[] teams=lines[1].split(",");
+        String[][] teams2= new String[getEquipos(input)][getEquipos(input)];
+        for (int i = 0; i <teams.length ; i++) {
+           teams2[i] = teams[i].split(" ");
+        }
+        return teams2;
+        }
 
+    public String[][] allmatches(String input){
+        String[] lines=Leector(input);
+        String [] matches=lines[2].split(",");
+        String[] [] matches2= new String[getPartidos(input)][getPartidos(input)];
+        for (int i = 0; i < matches.length; i++) {
+            matches2[i]=matches[i].split(" ");
+        }
+        return matches2;
+    }
 }
+
+
+
+
+
